cc850c7203017114e03632203e44d6aebbb03e56
tohbay <tobechukwuobitube@gmail.com>
2025-03-19 16:09:36 +0100

    - setup react app with vite - setup folder structure with personal information feature folder

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import PersonalInformation from "./components/personal-information/PersonalInformation";
+
+function App() {
+  return <PersonalInformation />;
+}
+
+export default App;
---
+++ src/components/personal-information/PersonalInformation.tsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const PersonalInformation = () => {
+  return (
+    <div>
+      <div>
+        <h1>Personal Information</h1>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInformation;
